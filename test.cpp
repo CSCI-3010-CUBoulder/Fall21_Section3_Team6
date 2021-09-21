95a6f2b9644d4964589e96400cc849b15490a56d
MichaelZigray <59930528+MichaelZigray@users.noreply.github.com>
2021-09-21 13:57:31 -0600

    added product test

--- test.cpp
+++ test.cpp
@@ -10,3 +10,10 @@ TEST_CASE ("Testing sum functions", "[sum]") {
     std::vector<int> v2{30, 21, 2, -10};
     REQUIRE(Sum(v2) == 43);
 }
+
+TEST_CASE ("Testing product functions", "[product]") {
+    std::vector<int> v1{2, 4, 6, -1};
+    REQUIRE(Product(v1) == -48);
+    std::vector<int> v2{0, 12, 19};
+    REQUIRE(Product(v2) == 0);
+}
